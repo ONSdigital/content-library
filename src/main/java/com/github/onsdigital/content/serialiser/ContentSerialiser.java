9d0d4353334d068eec95972f28a26f1706d7bf31
Bren <brneser@gmail.com>
2015-06-09 16:41:45 +0100

    Fixed content serialiser does not return approperiate object type

--- src/main/java/com/github/onsdigital/content/serialiser/ContentSerialiser.java
+++ src/main/java/com/github/onsdigital/content/serialiser/ContentSerialiser.java
@@ -1,5 +1,6 @@
 package com.github.onsdigital.content.serialiser;
 
+import com.github.onsdigital.content.base.Content;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
@@ -8,10 +9,11 @@ import java.io.InputStreamReader;
 
 /**
  * Created by bren on 06/06/15.
- * <p>
+ *
  * Several utilities to serialise/deserialize content types.
- * <p>
+ *
  * Uses d MMM yyy as default date format for date fields. (e.g. 1 Jan 2015, 10 Feb 2015)
+ *
  */
 public class ContentSerialiser {
 
@@ -30,23 +32,22 @@ public class ContentSerialiser {
 
     /**
      * Returns json string for given object
-     *
      * @return
      */
     public String serialise(Object object) {
         return gson.toJson(object);
     }
 
-    public Object deserialise(String json, Class type) {
+    public <O extends Object> O deserialise(String json, Class<O> type) {
         return gson.fromJson(json, type);
     }
 
-    public Object deserialise(InputStream stream, Class type) {
+    public <O extends Object> O deserialise(InputStream stream, Class<O> type) {
         return gson.fromJson(new InputStreamReader(stream), type);
     }
 
     private GsonBuilder createBuilder() {
-        return createBuilder(DEFAULT_DATE_PATTERN);
+        return createBuilder( DEFAULT_DATE_PATTERN);
     }
 
     private GsonBuilder createBuilder(String datePattern) {
