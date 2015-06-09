71af03d5a03d3fb6dcb635b74de2416e139bc51e
Bren <brneser@gmail.com>
2015-06-09 18:09:53 +0100

    Content type class mapping

---
+++ src/main/java/com/github/onsdigital/content/Unknown.java
@@ -0,0 +1,13 @@
+package com.github.onsdigital.content;
+
+import com.github.onsdigital.content.base.Content;
+import com.github.onsdigital.content.base.ContentType;
+
+/**
+ * Created by bren on 09/06/15.
+ */
+public class Unknown extends Content {
+    public Unknown(ContentType type) {
+        super(type);
+    }
+}
