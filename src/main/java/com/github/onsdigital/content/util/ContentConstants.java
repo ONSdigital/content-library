d2ed38c9fb7c54a8561dedcec97205edb14fec85
Bren <brneser@gmail.com>
2015-06-16 15:59:24 +0100

    Added millisecond precision in date format due to incompatibility with javascript isoString function

--- src/main/java/com/github/onsdigital/content/util/ContentConstants.java
+++ src/main/java/com/github/onsdigital/content/util/ContentConstants.java
@@ -9,7 +9,7 @@ import java.net.URI;
  */
 public class ContentConstants {
 
-    public static final String JSON_DATE_PATTERN = DateFormatUtils.ISO_DATETIME_TIME_ZONE_FORMAT.getPattern();
+    public static final String JSON_DATE_PATTERN = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
     public  static final URI HOME_URI = URI.create("/");
     public  static final String HOME_TITLE = "Home";
 
