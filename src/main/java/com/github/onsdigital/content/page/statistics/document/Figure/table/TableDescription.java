7c9b933b7072141fbfb8ff0f0e6e17105698765c
CarlHembrough <carlhuk@gmail.com>
2015-06-23 11:08:54 +0100

    Added initial version of chart and table page classes.

---
+++ src/main/java/com/github/onsdigital/content/page/statistics/document/Figure/table/TableDescription.java
@@ -0,0 +1,6 @@
+package com.github.onsdigital.content.page.statistics.document.Figure.table;
+
+import com.github.onsdigital.content.page.base.PageDescription;
+
+public class TableDescription extends PageDescription {
+}
