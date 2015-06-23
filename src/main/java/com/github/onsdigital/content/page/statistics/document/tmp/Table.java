673f1e4c2ad42bf973dd5f3d7861da012b6e63e6
CarlHembrough <carlhuk@gmail.com>
2015-06-23 11:26:49 +0100

    temp commit fixing casing issues.

---
+++ src/main/java/com/github/onsdigital/content/page/statistics/document/tmp/Table.java
@@ -0,0 +1,31 @@
+package com.github.onsdigital.content.page.statistics.document.tmp;
+
+import com.github.onsdigital.content.page.base.Page;
+import com.github.onsdigital.content.page.base.PageType;
+
+public class Table extends Page<TableDescription> {
+
+    private String title;
+    private String html;
+
+    @Override
+    public PageType getType() {
+        return PageType.table;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getHtml() {
+        return html;
+    }
+
+    public void setHtml(String html) {
+        this.html = html;
+    }
+}
