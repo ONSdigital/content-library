48fa9f37daa1a8f0bea295a8eafd6c7e67005bcd
CarlHembrough <carlhuk@gmail.com>
2015-06-23 11:28:18 +0100

    temp commit fixing casing issues.

--- src/main/java/com/github/onsdigital/content/page/statistics/document/Figure/table/TableDescription.java
+++
@@ -1,6 +0,0 @@
-package com.github.onsdigital.content.page.statistics.document.Figure.table;
-
-import com.github.onsdigital.content.page.base.PageDescription;
-
-public class TableDescription extends PageDescription {
-}
