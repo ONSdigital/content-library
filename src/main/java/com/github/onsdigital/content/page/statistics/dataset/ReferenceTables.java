c2e92788713f3fc171850ee453367374c9792f3b
Bren <brneser@gmail.com>
2015-07-01 17:42:56 +0100

    References tables page migrated flag

--- src/main/java/com/github/onsdigital/content/page/statistics/dataset/ReferenceTables.java
+++ src/main/java/com/github/onsdigital/content/page/statistics/dataset/ReferenceTables.java
@@ -7,8 +7,19 @@ import com.github.onsdigital.content.page.statistics.base.Statistics;
  * Created by bren on 01/07/15.
  */
 public class ReferenceTables extends Dataset {
+
+    private boolean migrated;
+
     @Override
     public PageType getType() {
         return PageType.reference_tables;
+    }
+
+    public boolean isMigrated() {
+        return migrated;
+    }
+
+    public void setMigrated(boolean migrated) {
+        this.migrated = migrated;
     }
 }
