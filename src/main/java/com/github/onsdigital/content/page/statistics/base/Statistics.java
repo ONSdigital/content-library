c87e65355fd6dd1e487514a4f8cc659e5f400d75
CarlHembrough <carlhuk@gmail.com>
2015-07-07 15:18:14 +0100

    Merge branch 'master' of /Users/carlhembrough/dev/code/content-library with conflicts.

--- src/main/java/com/github/onsdigital/content/page/statistics/base/Statistics.java
+++ src/main/java/com/github/onsdigital/content/page/statistics/base/Statistics.java
@@ -1,7 +1,6 @@
 package com.github.onsdigital.content.page.statistics.base;
 
 import com.github.onsdigital.content.page.base.Page;
-import com.github.onsdigital.content.page.base.PageDescription;
 
 /**
  * Created by bren on 04/06/15.
@@ -9,6 +8,6 @@ import com.github.onsdigital.content.page.base.PageDescription;
  * <p>
  * Represents statistics pages that gets released periodically. Bulletin, Article, Timeseries, Dataset, etc.
  */
-public abstract class Statistics extends Page {
+public abstract class Statistics<T extends StatisticsDescription> extends Page<T> {
 
 }
