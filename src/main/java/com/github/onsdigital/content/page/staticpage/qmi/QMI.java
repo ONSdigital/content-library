9ea603530c5e2521b642b304af0b82f6e020d252
Bren <brneser@gmail.com>
2015-08-07 12:09:46 +0100

    Added  releatedDatasets and relatedDocuments to qmi

--- src/main/java/com/github/onsdigital/content/page/staticpage/qmi/QMI.java
+++ src/main/java/com/github/onsdigital/content/page/staticpage/qmi/QMI.java
@@ -1,14 +1,37 @@
 package com.github.onsdigital.content.page.staticpage.qmi;
 
+import com.github.onsdigital.content.link.PageReference;
 import com.github.onsdigital.content.page.base.PageType;
 import com.github.onsdigital.content.page.staticpage.base.BaseStaticPage;
+
+import java.util.List;
 
 /**
  * Created by bren on 04/06/15.
  */
 public class QMI extends BaseStaticPage {
+
+    private List<PageReference> relatedDatasets;
+    private List<PageReference> relatedDocuments;
+
     @Override
     public PageType getType() {
         return PageType.static_qmi;
+    }
+
+    public List<PageReference> getRelatedDatasets() {
+        return relatedDatasets;
+    }
+
+    public void setRelatedDatasets(List<PageReference> relatedDatasets) {
+        this.relatedDatasets = relatedDatasets;
+    }
+
+    public List<PageReference> getRelatedDocuments() {
+        return relatedDocuments;
+    }
+
+    public void setRelatedDocuments(List<PageReference> relatedDocuments) {
+        this.relatedDocuments = relatedDocuments;
     }
 }
